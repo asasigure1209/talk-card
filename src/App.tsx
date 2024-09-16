468d2dfa24e883b47c1ee627fe0a23faf66f91b4
asasigure <mkmokymh@gmail.com>
2024-09-16 11:11:29 +0900

    add tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import "./index.css";
 
 function App() {
   return (
     <div>
-      <h1>React App</h1>
+      <h1 className="text-3xl font-bold underline">React App</h1>
     </div>
   );
 }
