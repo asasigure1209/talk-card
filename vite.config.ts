c965b9354d8460284779daf4b1fb7d047415cf6f
asasigure <mkmokymh@gmail.com>
2024-09-21 14:24:23 +0900

    setting tanstack router on vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), react()],
+});
