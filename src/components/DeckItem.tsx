a1dc223fd18a415237e9080d77d7ee1dec9a6052
asasigure <mkmokymh@gmail.com>
2024-09-29 06:35:22 +0900

    Comment out until implemented

--- src/components/DeckItem.tsx
+++ src/components/DeckItem.tsx
@@ -18,7 +18,7 @@ function DeckItem({ deckId, talkTheme }: DeckItem) {
       className="bg-white shadow-md rounded-lg p-4 w-40 h-56 flex items-center justify-center relative"
     >
       <span className="text-lg font-bold">{talkTheme}</span>
-      <button
+      {/* <button
         onClick={() => setIsShowDetail(!isShowDetail)}
         type="button"
         className="absolute top-4 right-2 h-5 w-5 flex flex-col items-center space-y-1"
@@ -26,7 +26,7 @@ function DeckItem({ deckId, talkTheme }: DeckItem) {
         <span className="block w-1 h-1 bg-gray-500 rounded-full" />
         <span className="block w-1 h-1 bg-gray-500 rounded-full" />
         <span className="block w-1 h-1 bg-gray-500 rounded-full" />
-      </button>
+      </button> */}
       {isShowDetail && (
         <div className="absolute top-2 right-7 flex flex-col text-sm bg-white shadow p-1 rounded">
           <button type="button" className="text-blue-500">
