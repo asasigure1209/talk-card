5f5c7dcd6d5b3aaddc53fc6218cf0daa8bc17c30
asasigure <mkmokymh@gmail.com>
2024-10-16 00:02:03 +0900

    del: unused backend

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,11 +1,6 @@
-import { defineBackend } from '@aws-amplify/backend';
-import { auth } from './auth/resource';
-import { data } from './data/resource';
+import { defineBackend } from "@aws-amplify/backend";
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
  */
-defineBackend({
-  auth,
-  data,
-});
+defineBackend({});
